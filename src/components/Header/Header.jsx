fb264f7e5b08708b534b29f21803d4f2cbf49c38
EPeng <erpengic@gmail.com>
2021-11-09 17:28:27 -0600

    changed the text input to controlled component instead of using useRef hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,44 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar, Typography, TextField, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
 import useStyles from "./styles";
 import { Button } from "@mui/material";
 
-import { fetchSearchData } from "../../api";
+import { fetchPageData } from "../../api";
 
 const Header = ({ setSearchValue, setProducts }) => {
-  const searchText = useRef("");
+  const [searchText, setSearchText] = useState("");
   const classes = useStyles();
-  const handleClick = async (e) => {
+
+  console.log(searchText);
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const products = await fetchSearchData(searchText.current.value);
+    const products = await fetchPageData(searchText, 1);
     setProducts(products);
-    setSearchValue(searchText.current.value);
+    setSearchValue(searchText);
     console.log(products);
   };
+
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
           Search API Assessment
         </Typography>
-        <Box component="form" display="flex">
+        <Box component="form" display="flex" onSubmit={handleSubmit}>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
             </div>
-            {/* <InputBase
-              placeholder="Search..."
-              classes={{ root: classes.inputRoot, input: classes.inputInput }}
-            /> */}
             <TextField
               id="search-text"
               label="Search..."
               variant="filled"
-              inputRef={searchText}
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
             />
-            <Button onClick={handleClick} className={classes.button}>
+            <Button type="submit" className={classes.button}>
               Submit
             </Button>
           </div>
